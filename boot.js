af842a140b934199380b8364e8b2de97b9e08e03
roux <52894745+sudocode1@users.noreply.github.com>
2021-01-26 19:22:44 +0000

    1.0 bruh moment

--- boot.js
+++ boot.js
@@ -5,13 +5,19 @@ const fetch = require("node-fetch");
 let reg = require(`./registry.json`);
 
 // data
-let version = "alpha-0.8";
-let blueprint = true;
-
-// functions
-
+let version = Buffer.from(reg.DATA["VERSION"], "base64").toString();
+let date = Buffer.from(reg.DATA["DATE"], "base64").toString();
+let blueprint = reg.DATA["BLUEPRINT"];
 
 // boot
+try {fs.readdirSync(`./commands`)} catch(e) {fs.mkdirSync(`commands`)};
+try {fs.readdirSync(`./drives`)} catch(e) {fs.mkdirSync(`drives`)};
+try {fs.readdirSync(`./boot`)} catch(e) {fs.mkdirSync(`boot`)};
+try {fs.readdirSync(`./shutdown`)} catch(e) {fs.mkdirSync(`shutdown`)};
+try {fs.readdirSync(`./bus`)} catch(e) {fs.mkdirSync(`bus`)};
+try {fs.readFileSync(`./bus/recentcmds.txt`, `utf-8`)} catch(e) {fs.writeFileSync(`./bus/recentcmds.txt`, ``)};
+
+
 function clear() {
     console.clear();
     console.log(` Welcome to NDOS ${version}`);
@@ -31,9 +37,15 @@ function boot() {
     reg.BOOT["DISABLE_DEVELOPER_NOTES"] !== 0 ? devNotes = false : null;
 
     if (devNotes == true) {
+        console.log("Join the Community Discord!\nReport bugs, suggest features and chat with other users!\nhttps://discord.gg/kg96m6AvpZ")
         console.log(`directories are currently disabled in mount/unmount (alpha-0.6.1 and above)`);
         console.log(`developed by sudocode1 and 1s3k3b`);
-        console.log(`Release Alpha-0.8, 1/25/2021`);
+        console.log(`Release ${version}, ${date}`);
+    }
+
+    if (reg.DATA.MODIFICATION["MODIFIED"] === true) {
+        console.log(`This build of NDOS is modified`);
+        console.log(`Modified by: ${reg.DATA.MODIFICATION["MODIFIED_BY"]}`)
     }
 
     if (bootscripts === true) {
@@ -51,10 +63,11 @@ boot();
 
 // script
 async function nd() {
+    // cmd split
     let cmd = prompt(`ndos>`);
-
     let spl = cmd.split(" ");
 
+    // commands
     if (cmd.startsWith(`help`)) {
         let cmds = [];
         let c = [];
@@ -63,7 +76,7 @@ async function nd() {
 
         cmds.map(x => {
             if (x[1][0] == 1) c.push([x[1][1], x[1][2]]); 
-        })
+        });
 
         console.log(`Commands in NDOS ${version}`);
         c.map(x => console.log(`${x[0]}: ${x[1]}`));
@@ -74,7 +87,17 @@ async function nd() {
         catch (e) {console.log(`There was an error accessing this directory.`)};
     }
 
-    else if (cmd.startsWith(`exit`)) process.exit()
+    else if (cmd.startsWith(`exit`)) {
+        let scripts = true;
+        if (reg.SHUTDOWN["DISABLE_SHUTDOWN_SCRIPTS"] !== 0) {scripts = false;};
+        if (scripts == true) {
+            fs.readdirSync(`./shutdown`).map(x => {
+                eval(fs.readFileSync(`./shutdown/${x}`, `utf-8`));
+            });
+
+        }
+        process.exit();
+    }
 
     else if (cmd.startsWith(`clear`)) {clear()}
 
@@ -190,23 +213,43 @@ async function nd() {
     }
 
     else if (cmd.startsWith(`jpac`)) {
-        await fetch(`http://ndosrepos.7m.pl/${spl[1]}.js`)
-        .then(res => res.text())
-        .then(body => fs.writeFileSync(`./commands/${spl[1]}.js`, body));
+        let cont = true;
+        if (reg.DATA["GO_ONLINE"] == false) {cont = false; console.log(`DATA.GO_ONLINE is disabled! Change it in the registry.`)};
+
+        if (cont == true) {
+            try {
+                await fetch(`http://ndosrepos.7m.pl/${spl[1]}.js`)
+                .then(res => res.text())
+                .then(body => fs.writeFileSync(`./commands/${spl[1]}.js`, body));
+            } catch(e) {
+                console.log("There was most likely a network error.");
+            }
+
+
+        }
         
     }
 
     else if (cmd.startsWith(`pacman`)) {
         let address = spl[1];
+        let cont = true;
+        if (reg.DATA["GO_ONLINE"] == false) {cont = false; console.log(`DATA.GO_ONLINE is disabled! Change it in the registry.`)};
+        
 
-        if (spl[1] == `default`) {
-            address = reg.PACMAN_CMD["DEFAULT_ADDRESS"];
-            console.log(`Using default address: ${reg.PACMAN_CMD["DEFAULT_ADDRESS"]}\nChange in registry options.`);
+        if (cont == true) {
+            if (spl[1] == `default`) {
+                address = reg.PACMAN_CMD["DEFAULT_ADDRESS"];
+                console.log(`Using default address: ${reg.PACMAN_CMD["DEFAULT_ADDRESS"]}\nChange in registry options.`);
+            }
+    
+            try {
+                await fetch(`http://${address}/${spl[2]}.js`)
+                .then(res => res.text())
+                .then(body => fs.writeFileSync(`./commands/${spl[2]}.js`, body))
+            } catch (e) {
+                console.log("There was most likely a network error.");
+            }
         }
-
-        await fetch(`http://${address}/${spl[2]}.js`)
-        .then(res => res.text())
-        .then(body => fs.writeFileSync(`./commands/${spl[2]}.js`, body))
     }
 
     else if (cmd.startsWith(`echo`)) {
@@ -217,9 +260,34 @@ async function nd() {
         eval(spl.slice(1).join(" "));
     }
 
+    else if (cmd.startsWith(`bus`)) {
+
+        switch(spl[1]) {
+            case "read":
+                console.log(fs.readFileSync(`./bus/recentcmds.txt`, `utf-8`));
+            break;
+
+            case "clear":
+                console.log(fs.writeFileSync(`./bus/recentcmds.txt`, ``));
+            break;
+
+            case "write": 
+                console.log(fs.appendFileSync(`./bus/recentcmds.txt`, `${spl.slice(2).join(" ")}\n`));
+            break;
+        }
+    }
+
+    // custom commands
     else {
         try {fs.readdirSync(`./commands`)} catch(e) {console.log(`This command does not exist!`)};
         fs.readdirSync(`./commands`).includes(`${cmd}.js`) ? eval(fs.readFileSync(`./commands/${cmd}.js`, `utf-8`)) : console.log("This command does not exist!");
+    }
+
+    // bus
+    if (reg.BUS["DISABLE_CMD_LOGGING"] !== 1) {
+        fs.appendFileSync(`./bus/recentcmds.txt`, `${cmd} ${Date.now()}\n`);
+
+        if (reg.BUS["DISABLE_CMD_LOGGING_NOTIFICATION"] !== 1) console.log(`BUS_CMD_LOGGING has logged ${cmd} to /bus/recentcmds.txt`);
     }
 
     console.log();
